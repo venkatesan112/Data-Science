39df23db133ceb01e9bcb7852815c8fa9ea2d4f3
venkatesan112 <venkiiv168@gmail.com>
2024-11-22 09:12:57 +0530

    Update number guess.py
    i add a new parameter count to try i have made to find the new number

--- number guess.py
+++ number guess.py
@@ -1,13 +1,17 @@
-def guess(a):
-  n=int(input())
+def guess(a,count):
+  n=int(input("Enter the Guess : "))
   if(n==a):
     print("you guess it right")
+    print(a,count)
   elif(n>a):
     print("Try a small number")
-    guess(a)
+    count+=1
+    guess(a,count)
   else:
     print("try a Big number")
-    guess(a)
+    count+=1
+    guess(a,count)
 import random
 a=random.randint(1,100)
-guess(a)
+count=1
+guess(a,count)
